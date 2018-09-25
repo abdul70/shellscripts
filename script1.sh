062fe4c4ab0db7c90d37cb2d75276ae661a53628
Abdul Ahad <abdul.ahad@triosstudent.com>
2018-09-25 11:19:13 -0400

    Made change to script1

--- script1.sh
+++ script1.sh
@@ -6,9 +6,11 @@ echo "Disk report saved to $FILENAME.report"
 
 echo -e "\n LVM Configuration: \n\n" >>$FILENAME.report
 lvscan >>$FILENAME.report
+vgscan >>$FILENAME.report
+pvscan >>$FILENAME.report
 
 echo -e "\n\n Partition Configuration: \n\n" >>$FILENAME.report
 fdisk -l | head -16 >>$FILENAME.report
 
-echo -e "\n\n Mounted Filesystems: \n\n" >>$FILENAME.report
+echo -e "\n\n Mounted Filesystems Usage: \n\n" >>$FILENAME.report
 df | grep -v tmp >>$FILENAME.report
